3817def321e642ed437a935693c8f1fc4e0180ad
alon <86466590+alongidron@users.noreply.github.com>
2024-11-18 20:25:23 +0200

    Delete terraform/lb directory

--- terraform/lb/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_lb" "app_lb" {
-  name                = "${var.project_name}-lb-${var.environment}"
-  location            = var.region
-  resource_group_name = var.project_name
-}
-
-output "lb_dns_name" {
-  value = azurerm_lb.app_lb.id
-}
