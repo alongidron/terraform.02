e023d35c88372546f39fa7d68d61587ad944a91f
alon <86466590+alongidron@users.noreply.github.com>
2024-11-18 20:25:46 +0200

    Delete terraform/modules/web_server directory

--- terraform/modules/web_server/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_linux_virtual_machine" "app" {
-  name                = "${var.project_name}-web-server-${var.environment}"
-  resource_group_name = var.project_name
-  location            = var.region
-  size                = var.instance_type
-}
